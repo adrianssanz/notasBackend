448e571bf4b1d9d810f683f9e9a4095161d86d7d
Adrian Sanz <sanzadrian@hotmail.es>
2024-12-20 13:14:24 +0100

    Agregar mensajes de inicio para la aplicación en consola

--- src/main/java/com/adriansanz/notasBackend/NotasBackendApplication.java
+++ src/main/java/com/adriansanz/notasBackend/NotasBackendApplication.java
@@ -8,6 +8,9 @@ public class NotasBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NotasBackendApplication.class, args);
+		System.out.println("--------------------------------------------------------------");
+		System.out.println("Aplicación iniciada en la ruta http://localhost:8080/api/notas");
+		System.out.println("--------------------------------------------------------------");
 	}
 
 }
