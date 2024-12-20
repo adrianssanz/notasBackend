0e2c88af26662a731a500da7c09fe66f3074ad5c
Adrian Sanz <sanzadrian@hotmail.es>
2024-12-20 23:06:15 +0100

    Agregar manejo de excepciones para rutas no encontradas y actualizar la configuración de recursos en application.properties

--- src/main/java/com/adriansanz/notasBackend/excepciones/manejadorExcepciones.java
+++ src/main/java/com/adriansanz/notasBackend/excepciones/manejadorExcepciones.java
@@ -4,11 +4,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @ControllerAdvice
 public class manejadorExcepciones {
     @ExceptionHandler(notaNoEncontradaException.class)
     public ResponseEntity<String> manejarNotaNoEncontradaException(notaNoEncontradaException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);  // Devuelve el mensaje con el código de estado 404
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<String> manejarRutaNoEncontrada(NoHandlerFoundException ex){
+        return new ResponseEntity<>("Ruta no encontrada: " + ex.getRequestURL(), HttpStatus.NOT_FOUND);
     }
 }
