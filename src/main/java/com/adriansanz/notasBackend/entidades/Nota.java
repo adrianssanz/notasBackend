9a91a9eea09ce61e72fa8fe81aed72056f0b8ce8
Adrian Sanz <sanzadrian@hotmail.es>
2024-12-21 16:52:56 +0100

    Agregar anotaciones de columna y corregir mensajes de validación en la entidad Nota

--- src/main/java/com/adriansanz/notasBackend/entidades/Nota.java
+++ src/main/java/com/adriansanz/notasBackend/entidades/Nota.java
@@ -1,5 +1,6 @@
 package com.adriansanz.notasBackend.entidades;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -11,13 +12,18 @@ import jakarta.validation.constraints.Size;
 public class Nota {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)  // Esto hace que el ID sea autogenerado
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
     private Long id;
+
     @NotEmpty(message = "El título no puede estar vacío")
-    @Size(max = 20, message = "El título no puede tener más de 10 caracteres")
+    @Size(max = 20, message = "El título no puede tener más de 20 caracteres")
+    @Column(name="titulo")
     private String titulo;
+
     @NotEmpty(message = "La descripción no puede estar vacía")
     @Size(max = 100, message = "La descripción no puede tener más de 10 caracteres")
+    @Column(name="descripcion")
     private String descripcion;
 
     // Constructores, getters y setters
