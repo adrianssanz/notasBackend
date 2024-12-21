eba1f244f3b3074601856b3787daad1eb4fffb78
Adrian Sanz <sanzadrian@hotmail.es>
2024-12-21 17:03:48 +0100

    Agregar Lombok a la entidad Nota y simplificar el código eliminando constructores y métodos getter/setter

--- src/main/java/com/adriansanz/notasBackend/entidades/Nota.java
+++ src/main/java/com/adriansanz/notasBackend/entidades/Nota.java
@@ -4,15 +4,18 @@ import java.time.LocalDateTime;
 
 import org.hibernate.annotations.CreationTimestamp;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data 
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "notas")
 public class Nota {
 
     @Id
@@ -33,44 +36,4 @@ public class Nota {
     @CreationTimestamp
     @Column(name = "fecha_creacion", updatable = false)
     private LocalDateTime fechaCreacion;
-
-    // Constructores, getters y setters
-    public Nota() {}
-
-    public Nota(String titulo, String descripcion) {
-        this.titulo = titulo;
-        this.descripcion = descripcion;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
-    public LocalDateTime getFechaCreacion() {
-        return fechaCreacion;
-    }
-
-    public void setFechaCreacion(LocalDateTime fechaCreacion) {
-        this.fechaCreacion = fechaCreacion;
-    }
 }
