25d3f132bd6d22d9a53fcd14d6a5e607245b268a
Adrian Sanz <sanzadrian@hotmail.es>
2024-12-30 20:20:12 +0100

    Agregar inyección de dependencia para RolRepositorio y asignar rol por defecto al registrar usuario

--- src/main/java/com/adriansanz/notasBackend/servicios/impl/AuthServicioImpl.java
+++ src/main/java/com/adriansanz/notasBackend/servicios/impl/AuthServicioImpl.java
@@ -9,9 +9,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.adriansanz.notasBackend.dto.UsuarioDTO;
+import com.adriansanz.notasBackend.entidades.Rol;
 import com.adriansanz.notasBackend.entidades.Usuario;
 import com.adriansanz.notasBackend.excepciones.usuarioDuplicadoException;
 import com.adriansanz.notasBackend.mappers.UsuarioMapper;
+import com.adriansanz.notasBackend.repositorios.RolRepositorio;
 import com.adriansanz.notasBackend.repositorios.UsuarioRepositorio;
 import com.adriansanz.notasBackend.servicios.AuthServicio;
 
@@ -20,6 +22,12 @@ public class AuthServicioImpl implements AuthServicio {
 
     @Autowired
     private UsuarioRepositorio usuarioRepositorio;
+    private RolRepositorio rolRepositorio;
+
+    public AuthServicioImpl(UsuarioRepositorio usuarioRepositorio, RolRepositorio rolRepositorio) {
+        this.usuarioRepositorio = usuarioRepositorio;
+        this.rolRepositorio = rolRepositorio;
+    }
 
     @Override
     public boolean loginUsuario(String usuario, String password) {
@@ -43,6 +51,9 @@ public class AuthServicioImpl implements AuthServicio {
 
         String encryptedPassword = new BCryptPasswordEncoder().encode(usuario.getPassword());
         usuario.setPassword(encryptedPassword);
+        
+        Rol rol = rolRepositorio.findById(1L).get();
+        usuario.setRol(rol);
 
         usuarioRepositorio.save(usuario);
 
