9ae08f86e88f3603d5bc3b3fedb8c1946acc77c8
Adrian Sanz <sanzadrian@hotmail.es>
2025-01-08 18:46:44 +0100

    Agregar validación de permisos de usuario en el controlador de usuarios

--- src/main/java/com/adriansanz/notasBackend/controllers/UsuarioController.java
+++ src/main/java/com/adriansanz/notasBackend/controllers/UsuarioController.java
@@ -13,33 +13,54 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.adriansanz.notasBackend.dto.UsuarioDTO;
+import com.adriansanz.notasBackend.entidades.Usuario;
+import com.adriansanz.notasBackend.excepciones.noAutorizadoException;
+import com.adriansanz.notasBackend.servicios.AuthServicio;
 import com.adriansanz.notasBackend.servicios.UsuarioServicio;
+
+import jakarta.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("/api/usuarios")
 public class UsuarioController {
     @Autowired
     private UsuarioServicio usuarioServicio;
+    private AuthServicio authServicio;
+
+    public UsuarioController(AuthServicio authServicio){
+        this.authServicio = authServicio;
+    }
+
+    public void validarUsuarioAutenticado(HttpSession session){
+        Usuario usuario = authServicio.getSesionUsuario(session);
+        if (usuario.getRol().getId() != 1) {
+            throw new noAutorizadoException("No tienes permisos para realizar esta acción.");
+        }
+    }
 
     @GetMapping
     public ResponseEntity<List<UsuarioDTO>> getAllUsuarios(
             @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "5") int size) {
+            @RequestParam(defaultValue = "5") int size, HttpSession session) {
+        validarUsuarioAutenticado(session);
         return usuarioServicio.getAllUsuarios(page, size);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<UsuarioDTO> getUsuarioById(@PathVariable Long id) {
+    public ResponseEntity<UsuarioDTO> getUsuarioById(@PathVariable Long id, HttpSession session) {
+        validarUsuarioAutenticado(session);
         return usuarioServicio.getUsuarioById(id);
     }  
 
     @PutMapping("/{id}/rol")
-    public ResponseEntity<UsuarioDTO> updateRolUsuario(@PathVariable Long id){
+    public ResponseEntity<UsuarioDTO> updateRolUsuario(@PathVariable Long id, HttpSession session){
+        validarUsuarioAutenticado(session);
         return usuarioServicio.updateRolUsuario(id);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteUsuario(@PathVariable long id){
+    public ResponseEntity<Void> deleteUsuario(@PathVariable long id, HttpSession session){
+        validarUsuarioAutenticado(session);
         usuarioServicio.deleteUsuario(id);
         return ResponseEntity.noContent().build();
     }
